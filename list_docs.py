2216ecdb94a01ae370a67867e0f699c25aa47913
Thintsing <thintsing@users.noreply.github.com>
2026-08-21 16:35:20 +0800

    chore: remove temp scripts

--- list_docs.py
+++
@@ -1,9 +0,0 @@
-import urllib.request, json, sys
-sys.stdout.reconfigure(encoding="utf-8")
-
-url = "https://api.github.com/repos/GMOogway/shadowrocket-rules/contents/docs/"
-with urllib.request.urlopen(url) as resp:
-    for item in json.loads(resp.read().decode()):
-        name = item["name"]
-        dl = item["download_url"]
-        print(f"{name} -> {dl}")
